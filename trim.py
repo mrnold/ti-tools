d3828466dbe885d7e9f82cb2a5a349cd6b163e49
Matthew Arnold <5075485+mrnold@users.noreply.github.com>
2015-01-02 14:04:01 -0500

    Set global variables to zero.

--- trim.py
+++ trim.py
@@ -10,6 +10,7 @@ def main(args):
     class Pair(): pass
     initializer = Pair()
     initialized = Pair()
+    zeroglobals = Pair()
     offset = int(args[3], 16)
     with open(args[1]) as mapfile:
         for line in mapfile:
@@ -23,9 +24,20 @@ def main(args):
                 initialized.offset = int(parts[1], 16)-offset
                 initialized.size = int(parts[2], 16)
                 initialized.end = initialized.offset+initialized.size
+            elif line.startswith("_DATA"):
+                parts = line.split()
+                if len(parts) < 4: continue
+                zeroglobals.offset = int(parts[1], 16)-offset
+                zeroglobals.size = int(parts[2], 16)
+                zeroglobals.end = zeroglobals.offset+zeroglobals.size
 
-    print "Initialized: offset 0x{0:x} size {1}".format(initialized.offset, initialized.size)
-    print "Initializer: offset 0x{0:x} size {1}".format(initializer.offset, initializer.size)
+
+    print "Initialized: offset 0x",
+    print "{0:x} size {1}".format(initialized.offset, initialized.size)
+    print "Initializer: offset 0x",
+    print "{0:x} size {1}".format(initializer.offset, initializer.size)
+    print "Global variables: offset 0x",
+    print "{0:x} size {1}".format(zeroglobals.offset, zeroglobals.size)
 
     with open(args[2], "r+b") as binfile:
         mapped = mmap.mmap(binfile.fileno(), 0, access=mmap.ACCESS_WRITE)
@@ -33,6 +45,8 @@ def main(args):
         print "Data to move: {0}".format(binascii.hexlify(data))
         mapped.move(initialized.offset, initializer.offset, initializer.size)
         mapped.resize(mapped.size()-initialized.size)
+        mapped.seek(zeroglobals.offset)
+        mapped.write('\0'*zeroglobals.size)
         mapped.flush()
         mapped.close()
 
