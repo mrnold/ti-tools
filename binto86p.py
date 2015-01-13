ad91e2ccde6b7c1411902945922caaef51afb831
Matthew Arnold <5075485+mrnold@users.noreply.github.com>
2015-01-12 19:33:28 -0500

    Compact TI86 program generator somewhat.

--- binto86p.py
+++ binto86p.py
@@ -2,90 +2,39 @@ import os
 import sys
 import struct
 
+# TI-86 program variable passes:
+# Pass 1: Add 0x8E, 0x28 marker
+# Pass 2: Add length of program variable
+# Pass 3: 0x000C, program length, type ID, name length, name, program length
+# Pass 4: **TI86**, 0x1a, 0x0a, 0x00, comment, data section length
+# Pass 5: append checksum
 class Ti86(object):
     def __init__(self, programname, programdata, comment="No comment"):
         self.programname = os.path.basename(programname)
         print "Program name: {0}".format(self.programname)
         self.comment = comment
 
-        markedprogram = self.packmarkedprogram(programdata)
-        programvar = self.packprogramvar(markedprogram)
-        varentry = self.packvarentry(programvar)
-        program = self.packprogram(varentry)
-        final = self.packfinal(program)
+        passes = [
+                ("<H{0}s", lambda p: [0x288e, p]),
+                ("<H{0}s", lambda p: [len(p), p]),
+                ("<HHBB8sH{0}s", lambda p: [
+                    0x000C, len(p), 0x12, len(self.programname),
+                    self.programname.ljust(8, '\0')[0:8], len(p), p]),
+                ("<8s3B42sH{0}s", lambda p: [
+                    "**TI86**", 0x1a, 0x0a, 0x00,
+                    self.comment[0:42], len(p), p]),
+                ("<{0}sH", lambda p: [p, sum(bytearray(p)) & 0xFFFF])
+        ]
+
+        program = programdata
+        for structfmt, transform in passes:
+            length = len(program)
+            fmt = structfmt.format(length)
+            program = struct.pack(fmt, *transform(program))
 
         programfilename = programname+".86p"
         with open(programfilename, 'wb') as programfile:
-            programfile.write(final)
-
-    def pack(self, function):
-        fields, structformat = function()
-
-    MarkedFormat  = "<"    # Little-endian
-    MarkedFormat += "H"    # 0x8e, 0x28 marker
-    MarkedFormat += "{0}s"
-    def packmarkedprogram(self, programdata):
-        datalength = len(programdata)
-        fields = [0x288e, programdata]
-        structformat = Ti86.MarkedFormat.format(datalength)
-        return struct.pack(structformat, *fields)
-
-    ProgramVarFormat  = "<"    # Little-endian
-    ProgramVarFormat += "H"    # Variable length
-    ProgramVarFormat += "{0}s" # Assembled program
-    def packprogramvar(self, markedprogram):
-        datalength = len(markedprogram)
-        fields = [datalength, markedprogram]
-        structformat = Ti86.ProgramVarFormat.format(datalength)
-        return struct.pack(structformat, *fields)
-
-    VarEntryFormat  = "<"    # Little-endian
-    VarEntryFormat += "H"    # 0x000C
-    VarEntryFormat += "H"    # Assembled program length
-    VarEntryFormat += "B"    # Variable type ID
-    VarEntryFormat += "B"    # Variable name length
-    VarEntryFormat += "8s"   # Variable name
-    VarEntryFormat += "H"    # Assembled program length
-    VarEntryFormat += "{0}s" # Assembled program
-    def packvarentry(self, programvar):
-        datalength = len(programvar)
-        variablenamelen = len(self.programname)
-        variablename = self.programname.ljust(8, '\0')[0:8]
-        structformat = Ti86.VarEntryFormat.format(datalength)
-        print structformat
-        fields = [
-            0x000C, datalength,
-            0x12, variablenamelen,
-            variablename, datalength,
-            programvar
-        ]
-        return struct.pack(structformat, *fields)
-
-    ProgramFormat  = "<"    # Little-endian
-    ProgramFormat += "8s"   # **TI86**
-    ProgramFormat += "3B"   # 0x1a, 0x0a, 0x00
-    ProgramFormat += "42s"  # Comment
-    ProgramFormat += "H"    # Data section length
-    ProgramFormat += "{0}s" # Program data section
-    def packprogram(self, varentry):
-        variablelen = len(varentry)
-        structformat = Ti86.ProgramFormat.format(variablelen)
-        fields = [
-            "**TI86**", 0x1a, 0x0a, 0x00,
-            self.comment[0:42], variablelen, varentry
-        ]
-        return struct.pack(structformat, *fields)
-
-    FinalFormat  = "<"    # Little-endian
-    FinalFormat += "{0}s" # The rest of the program
-    FinalFormat += "H"    # File checksum
-    def packfinal(self, program):
-        programlen = len(program)
-        structformat = Ti86.FinalFormat.format(programlen)
-        checksum = sum(bytearray(program)) & 0xFFFF
-        fields = [program, checksum]
-        return struct.pack(structformat, *fields)
-
+            programfile.write(program)
 
 
 def main(args):
